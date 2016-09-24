b1ac7efb626f3fabc99b54346383f8afc035b107
dnearhos <david@nearhos.com>
2016-09-24 16:24:45 +1000

    First Update

---
+++ al-gorithim/al-gorithim.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : al-gorithim.cpp
+// Author      : David
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C, Ansi-style
+//============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World!!!");
+	return EXIT_SUCCESS;
+}
